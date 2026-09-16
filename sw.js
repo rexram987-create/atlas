7caea21e5d1f528c8bde1768ac15ff2928e821f6
rexram987-create <rexram987@gmail.com>
2026-09-16 15:29:03 +0300

    Bump Atlas cache for country facts

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'atlas-v3';
+const CACHE = 'atlas-v4';
 self.addEventListener('install', event => {
   event.waitUntil((async () => {
     const response = await fetch('/precache.json', {cache:'no-store'});
