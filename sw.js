f73f9d8ccd93db7a83f243bf46c372d83586ce87
rexram987-create <rexram987@gmail.com>
2026-09-16 13:11:35 +0300

    Cache country details in atlas-v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'atlas-v2';
+const CACHE = 'atlas-v3';
 self.addEventListener('install', event => {
   event.waitUntil((async () => {
     const response = await fetch('/precache.json', {cache:'no-store'});
