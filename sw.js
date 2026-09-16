1c1fb068bddc7e68e8a86109880eaec31d697451
rexram987-create <rexram987@gmail.com>
2026-09-16 18:42:42 +0300

    Bump Atlas cache for currency labels

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'atlas-v4';
+const CACHE = 'atlas-v5';
 self.addEventListener('install', event => {
   event.waitUntil((async () => {
     const response = await fetch('/precache.json', {cache:'no-store'});
