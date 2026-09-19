fdd2fcb83eba395ab270ece6446107139bfdcb0f
rexram987-create <rexram987@gmail.com>
2026-09-19 15:57:56 +0300

    Refresh PWA for Mongolia first verified history batch

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'atlas-v9';
+const CACHE = 'atlas-v10';
 self.addEventListener('install', event => {
   event.waitUntil((async () => {
     const response = await fetch('/precache.json', {cache:'no-store'});
