c0e1c6c6ff295242a1997cdc9dd67e377970f400
rexram987-create <rexram987@gmail.com>
2026-09-19 18:02:39 +0300

    Refresh installed atlas for history batch 2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'atlas-v10';
+const CACHE = 'atlas-v11';
 self.addEventListener('install', event => {
   event.waitUntil((async () => {
     const response = await fetch('/precache.json', {cache:'no-store'});
