599d4eb57d3d1f8eddedbaf82d4c8218477931a0
rexram987-create <rexram987@gmail.com>
2026-09-19 15:33:36 +0300

    Refresh PWA cache for compact history cards

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'atlas-v8';
+const CACHE = 'atlas-v9';
 self.addEventListener('install', event => {
   event.waitUntil((async () => {
     const response = await fetch('/precache.json', {cache:'no-store'});
