6b2e671d707ea5c6d1f11448e951dc794361b7fd
rexram987-create <rexram987@gmail.com>
2026-09-19 15:57:46 +0300

    Support alternate Ulan Bator capital spelling

--- facts.mjs
+++ facts.mjs
@@ -30,7 +30,7 @@ const HEBREW_CAPITAL_FALLBACKS = Object.freeze({
   JO: { Amman: 'עמאן' },
   OM: { Muscat: 'מסקט' },
   KZ: { Astana: 'אסטנה' },
-  MN: { Ulaanbaatar: 'אולן בטור' },
+  MN: { Ulaanbaatar: 'אולן בטור', 'Ulan Bator': 'אולן בטור' },
   KG: { Bishkek: 'בישקק' }
 });
 
