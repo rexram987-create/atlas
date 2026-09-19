b2849560cb2d070ef462a92e0bd8d86d8f16bdd6
rexram987-create <rexram987@gmail.com>
2026-09-19 14:42:37 +0300

    Bump PWA service worker cache for Hebrew capital fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'atlas-v6';
+const CACHE = 'atlas-v7';
 self.addEventListener('install', event => {
   event.waitUntil((async () => {
     const response = await fetch('/precache.json', {cache:'no-store'});
